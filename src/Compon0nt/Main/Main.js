63d86d5a80175d3b19564c9080970a34245e9b67
suzitsarkar27 <96899998+suzitsarkar27@users.noreply.github.com>
2022-03-26 12:59:16 +0600

    add-product

--- src/Compon0nt/Main/Main.js
+++ src/Compon0nt/Main/Main.js
@@ -11,14 +11,18 @@ const Main = () => {
     },[])
     return (
         <div className='Item-container'>
-            <div className="product-container">
+            <div className="product-container row">
                 <h2>Product</h2>
+                
                 {
                     item.map(items=><Product key={items.id} items={items}></Product>)
                 }
+            
             </div>
-            <div className="card-container">
-                 <h2>Sider container</h2>
+            <div className="card-container row">
+                <div className="col-12">
+                <h2>SIde container</h2>
+                 </div>
             </div>
         </div>
     );
--- src/Compon0nt/Product/Product.js
+++ src/Compon0nt/Product/Product.js
@@ -3,13 +3,18 @@ import './Product.css';
 
 const Product = (Props) => {
     // const {name,price,img } = Props.items;
-    const {name,img } = Props.items[0];
+    const {name,img ,price} = Props.items[0];
     return (
-        <div className="row">
-             <div className='Product-container col-md-4'>
-           <img src={img} alt="" />
+        <div className='Product-container col-md-3 g-5'>
+            <img src={img} alt="" />
+            <div className="product-infro">
+            <h2>Name:{name}</h2>
+            <h3><small>Price:{price}</small></h3>
+            </div>
+            <div className="product-button">
+            <button>Add To Card</button>
+            </div>
         </div>
-       </div>
     );
 };
 
