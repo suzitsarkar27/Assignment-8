c58bfcda2e25b266e7c01a2f7978452e79b4c8f8
suzitsarkar27 <96899998+suzitsarkar27@users.noreply.github.com>
2022-03-26 19:14:52 +0600

    solve

--- src/Compon0nt/Header.js
+++ src/Compon0nt/Header.js
@@ -7,9 +7,8 @@ const Header = () => {
  <nav class="navbar navbar-expand-lg navbar-light bg-light ">
  <div class="container-fluid ">
  <div class="container-fluid">
-<a class="navbar-brand" href="#">
-<img src="/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24" class="d-inline-block align-text-top"/>
-Bootstrap
+<a  class="navbar-brand text-danger" href="#">
+Iphone
 </a>
 </div>
 <div class="collapse navbar-collapse " id="navbarTogglerDemo01">
--- src/Compon0nt/Infro/Inpro.js
+++ src/Compon0nt/Infro/Inpro.js
@@ -4,7 +4,7 @@ import Questiom from '../Question/Questiom';
 const Inpro = () => {
     return (
         <div className='row'>
-            <div className="col md-12 text-center">
+            <div className="col md-12 text-center card">
                 <h2>How React Works</h2>
                 <p>Answer : React is a JavaScript library (not a framework) that creates user interfaces (UIs) in a predictable and efficient way using declarative code. You can use it to help build single page applications and mobile apps, or to build complex apps if you utilise it with other libraries.
                </p>
